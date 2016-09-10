0704ad061a39d0469ceceda52eeeef0e9522a63d
David Tverdota <david.tverdota@smartfocus.com>
2016-09-10 18:05:19 +0100

    Added image manipulation with compute shader

---
+++ ImageManipulator/app/src/main/java/imagemanipulator/CustomGLSurfaceView.java
@@ -0,0 +1,65 @@
+package imagemanipulator;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by David Tverdota on 7/24/2016.
+ */
+public class CustomGLSurfaceView extends GLSurfaceView {
+    CustomGLRenderer mRenderer;
+
+    CustomGLSurfaceView ( Context context ) {
+        super ( context );
+        mRenderer = new CustomGLRenderer(this);
+        setEGLContextClientVersion ( 3 );
+        setRenderer ( mRenderer );
+        setRenderMode ( GLSurfaceView.RENDERMODE_WHEN_DIRTY );
+    }
+
+    public void surfaceCreated ( SurfaceHolder holder ) {
+        super.surfaceCreated(holder);
+    }
+
+    public void surfaceDestroyed ( SurfaceHolder holder ) {
+        super.surfaceDestroyed(holder);
+    }
+
+    public void surfaceChanged ( SurfaceHolder holder, int format, int w, int h ) {
+        super.surfaceChanged(holder, format, w, h);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mRenderer.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mRenderer.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        float touchedX, touchedY;
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            touchedX = event.getX();
+            touchedY = event.getY();
+
+            mRenderer.setAligments(touchedX, touchedY);
+
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            touchedX = event.getX();
+            touchedY = event.getY();
+
+            mRenderer.setAligments(touchedX, touchedY);
+        }
+        return true;
+
+    }
+}
